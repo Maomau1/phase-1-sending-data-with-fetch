27948c6959f447b86ddb6f40ad02bbaf74a71141
maurice diong <mdiong00@gmail.com>
2023-10-17 10:21:24 -0400

    how to use fetch to post and modify DOM

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+//const element = document.body.createElement("div");
+function submitData (userName,userEmail) {
+const user = {
+    name: `${userName}`,
+    email:`${userEmail}`,
+};
+console.log(user);
+
+return(fetch ("http://localhost:3000/users",{
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        
+    },
+    body:JSON.stringify(user),
+        //name: `${userName}`,
+        //email:`${userEmail}`,
+    
+})
+.then(function (response) {
+    return response.json();
+})
+.then(function(object) {
+    console.log(object.id);
+    const element = document.createElement("div");
+    document.body.append(element);
+    element.textContent = `${object.id}`;
+})
+.catch((error) => {
+    alert("Bad things! Ragnarők!");
+    const element = document.createElement("div");
+    document.body.append(element);
+    element.textContent = `${error}`;
+    console.log(error.message);
+})
+)
+//document.querySelector()
+}
+
